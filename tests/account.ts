1d2449d7abc89e3d72d81313b1363b7b20b47482
kevin.tirta <kevin.tirta@okg.com>
2023-02-26 11:33:13 +0800

    feat: introduce ballot

---
+++ tests/account.ts
@@ -0,0 +1,9 @@
+// import * from "@nomicfoundation/hardhat-toolbox";
+
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
